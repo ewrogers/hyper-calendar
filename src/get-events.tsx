97de862522872b29b7ccd5adae9d666bad9fdc94
Erik Rogers <ewrogers@gmail.com>
2023-09-10 17:02:41 -0400

    add basic calendar layout

--- src/get-events.tsx
+++ src/get-events.tsx
@@ -1,3 +1,5 @@
+import addDays from 'date-fns/addDays'
+import formatDate from 'date-fns/format'
 import startOfWeek from 'date-fns/startOfWeek'
 
 import Layout from '@/layout'
@@ -6,13 +8,51 @@ interface EventTableProps {
     startDate: Date
 }
 
-const EventsTable = (props: EventTableProps) => (
-    <div>
-        <h1>The quick brown fox jumps over the lazy dog.</h1>
-        <h2>This is the events table placeholder.</h2>
-        <h3>{props.startDate.toString()}</h3>
-    </div>
-)
+const EventsTable = (props: EventTableProps) => {
+    const toShortDate = (date: Date, offset: number): string =>
+        formatDate(addDays(date, offset), 'M/dd')
+    const toDayOfWeek = (date: Date, offset: number): string =>
+        formatDate(addDays(date, offset), 'EEEE')
+
+    return (
+        <div class="container">
+            <div class="calendar-header">
+                <span>&lt; LAST WEEK</span>
+                <div class="spacer" />
+                <span>MONTH</span>
+                <span>YEAR</span>
+                <div class="spacer" />
+                <span>NEXT WEEK &gt;</span>
+            </div>
+            <table class="events-table">
+                <thead>
+                    <tr class="week-header">
+                        {
+                            // Loop through each day of the week
+                            // Somewhat a hack because you cannot use normal `for` loops in JSX
+                            Array(7)
+                                .fill(1)
+                                .map((el, i) => (
+                                    <th class="day-header">
+                                        <span>
+                                            {toShortDate(props.startDate, i)}
+                                        </span>
+                                        <span>
+                                            {toDayOfWeek(props.startDate, i)}
+                                        </span>
+                                    </th>
+                                ))
+                        }
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+            <h1>The quick brown fox jumps over the lazy dog.</h1>
+            <h2>This is the events table placeholder.</h2>
+            <h3>{props.startDate.toString()}</h3>
+        </div>
+    )
+}
 
 export default function getEvents(c) {
     const startDate = startOfWeek(new Date())
