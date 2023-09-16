ae3e39118ff51832dc70566d274958f0cc53bb03
Erik Rogers <ewrogers@gmail.com>
2023-09-15 23:57:05 -0400

    extract nav into component

--- src/components/WeekCalendar.tsx
+++ src/components/WeekCalendar.tsx
@@ -3,6 +3,7 @@ import addWeeks from 'date-fns/addWeeks'
 import format from 'date-fns/format'
 import { CalendarEvent } from '@/models/event.ts'
 import DayHeader from '@/components/DayHeader.tsx'
+import WeekNavToolbar from '@/components/WeekNavToolbar.tsx'
 
 export interface WeekCalendarProps {
   startDate: Date
@@ -12,9 +13,6 @@ export interface WeekCalendarProps {
 const WeekCalendar = (props: WeekCalendarProps) => {
   const monthName = format(props.startDate, 'MMMM')
   const year = format(props.startDate, 'yyyy')
-
-  const prevWeek = addWeeks(props.startDate, -1)
-  const nextWeek = addWeeks(props.startDate, 1)
 
   return (
     <div id="calendar" class="week-calendar">
@@ -29,43 +27,10 @@ const WeekCalendar = (props: WeekCalendarProps) => {
           </svg>
         </button>
         <div class="spacer" />
-        <div class="toolbar">
-          <button
-            id="prev-week-btn"
-            class="toolbar-button"
-            hx-get={`/events?date=${format(prevWeek, 'yyyy-MM-dd')}`}
-            hx-target="#calendar"
-            hx-push-url="true"
-          >
-            <svg viewBox="0 0 24 24" width="24" height="24">
-              <path
-                d={`M 15.41 7.41 L 14 6 L 8 12 L 14 18 L 15.41 16.59 L 10.83 12 Z`}
-              />
-            </svg>
-          </button>
-          <button
-            id="today-btn"
-            class="toolbar-button"
-            hx-get="/events"
-            hx-target="#calendar"
-            hx-push-url="true"
-          >
-            <span class="toolbar-button-label">Today</span>
-          </button>
-          <button
-            id="next-week-btn"
-            class="toolbar-button"
-            hx-get={`/events?date=${format(nextWeek, 'yyyy-MM-dd')}`}
-            hx-target="#calendar"
-            hx-push-url="true"
-          >
-            <svg viewBox="0 0 24 24" width="24" height="24">
-              <path
-                d={`M 8.59 16.59 L 10 18 L 16 12 L 10 6 L 8.59 7.41 L 13.17 12 Z`}
-              />
-            </svg>
-          </button>
-        </div>
+        <WeekNavToolbar
+          prevWeek={addWeeks(props.startDate, -1)}
+          nextWeek={addWeeks(props.startDate, 1)}
+        />
       </div>
       <div class="week-header">
         {Array.from({ length: 7 }).map((_, i) => (
